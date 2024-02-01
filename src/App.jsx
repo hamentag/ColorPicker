b33c9970b682ce573aa4362f803c4e3b3cf7a52a
Hamza Amentag <hamentag1@gmail.com>
2024-01-31 21:51:03 -0600

    updated: Add border to selected color

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,29 @@ import { useState } from 'react'
 const Color = ({color, setSelectedColor}) => {
   return (
     <div 
-      className={color} 
-      onClick={() => setSelectedColor(color)} 
+      className={`${color}`}
+      onClick={(event) => {
+        setSelectedColor(color);
+        document.querySelectorAll('#colors-list div').forEach(el => {
+          if(el.className.includes(' selected'))
+            el.className = el.className.replace(' selected', '');
+          else if(el == event.target)
+            el.className += ' selected';
+        });
+      }}    
      ></div>
   )
 }
 
-/* or using object deconstruction */
-// const Color = ({color}) => {
-//   return <div className={color}></div>
-// }
-
 const App = () => {
-  const [selectedColor, setSelectedColor] = useState("")
+  const [selectedColor, setSelectedColor] = useState("");
+  const [isSelected, setSelected] = useState(false);
 
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className={selectedColor}>{selectedColor}</div>
+        <div className={selectedColor}>{selectedColor}</div> 
       </div>
       <div id="colors-list">
         <Color color="yellow" setSelectedColor={setSelectedColor} />
